bd6b2c4c935114254a48eeab3ad386bcd34f72a4
Timothy Miamba <timal0361@gmail.com>
2025-03-18 11:44:17 +0200

    Bring back working

--- src/main/java/method_problems/ZeroMover.java
+++ src/main/java/method_problems/ZeroMover.java
@@ -5,8 +5,40 @@ import java.util.Arrays;
 public class ZeroMover {
     public static void main(String[] args) {
         int[] testArray = new int[]{1,2,0,0,4,0,5};
+//        zeroesToEnd(testArray);
         zeroesToEndBubbleSortVersion(testArray);
         System.out.println(Arrays.toString(testArray));
+    }
+
+    public static void zeroesToEnd(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+
+            // Assume the current position holds
+            // a non-zero element
+            int zeroIndex = i;
+
+            // Iterate through the unsorted portion
+            // to find the actual minimum
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] == 0) {
+
+                    // Update zeroIndex if a smaller element
+                    // is found
+                    zeroIndex = j;
+                }
+            }
+
+            // Move minimum element to its
+            // correct position
+            int temp = arr[i];
+            arr[i] = arr[zeroIndex];
+            arr[zeroIndex] = temp;
+        }
+    }
+
+    public static void zeroesToEndAlpha(int[] arr) {
+
     }
 
     public static void zeroesToEndBubbleSortVersion(int arr[]){
@@ -17,7 +49,7 @@ public class ZeroMover {
         for (i = 0; i < n - 1; i++) {
             swapped = false;
             for (j = 0; j < n - i - 1; j++) {
-                if (arr[j + 1] == 0) {
+                if (arr[j] > arr[j + 1] && arr[j + 1] == 0) {
 
                     temp = arr[j];
                     arr[j] = arr[j + 1];
@@ -30,6 +62,7 @@ public class ZeroMover {
                 break;
         }
     }
+
 
 }
 
