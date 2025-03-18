6060bad2a30c02bc4a3d8e49c086ba29c400f75b
Timothy Miamba <timal0361@gmail.com>
2025-03-18 09:52:41 +0200

    Add FizzBuzz method and test

---
+++ src/main/java/methods/FizzBuzz.java
@@ -0,0 +1,22 @@
+package methods;
+
+public class FizzBuzz {
+    public static String fizzBuzz(int number) {
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        }
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+        return String.valueOf(number);
+    }
+    public static void main(String[] args) {
+        System.out.println(fizzBuzz(3));
+        System.out.println(fizzBuzz(5));
+        System.out.println(fizzBuzz(15));
+        System.out.println(fizzBuzz(4));
+    }
+}
